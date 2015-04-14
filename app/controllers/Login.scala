d9049cfaf5c57be6963c87273dd73c29c07e82dc
da0shi <whit3.develop@gmail.com>
2015-04-14 23:41:00 +0900

    Get token for GitHub oauth by env

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -5,11 +5,13 @@ import play.api.mvc._
 import services.GitHub.GitHubOAuthProvider
 
 object Login extends Controller {
+  val clientId: String = System.getenv("GITHUB_CLIENT_ID")
+  val clientSecret: String = System.getenv("GITHUB_CLIENT_SECRET")
 
   def github = Action { request =>
     val protocol = if (request.secure) "https" else "http"
     val params: Map[String, String] = Map(
-      "client_id" -> "",
+      "client_id" -> clientId,
       "redirect_uri" -> "%s://%s/login/github/callback".format(protocol, request.host),
       "scope" -> Set("user", "repo").mkString(","),
       "state" -> scala.util.Random.alphanumeric.take(20).mkString
@@ -20,8 +22,8 @@ object Login extends Controller {
     val protocol = if (request.secure) "https" else "http"
     val code: String = request.getQueryString("code").getOrElse("")
     val params: Map[String, String] = Map(
-      "client_id" -> "",
-      "client_secret" -> "",
+      "client_id" -> clientId,
+      "client_secret" -> clientSecret,
       "code" -> code,
       "redirect_uri" -> "%s://%s/login/github/callback".format(protocol, request.host)
     )

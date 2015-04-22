705915fd0582230fcd6003275864b03db691122e
da0shi <whit3.develop@gmail.com>
2015-04-22 18:43:02 +0900

    Add facebook login routing

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -6,20 +6,22 @@ import services.github.GitHubOAuthProvider
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Login extends Controller {
-  val clientId: String = sys.env("GITHUB_CLIENT_ID")
-  val clientSecret: String = sys.env("GITHUB_CLIENT_SECRET")
+  val githubClientId: String = sys.env("GITHUB_CLIENT_ID")
+  val githubClientSecret: String = sys.env("GITHUB_CLIENT_SECRET")
+  val facebookClientId: String = sys.env("FACEBOOK_CLIENT_ID")
+  val facebookClientSecret: String = sys.env("FACEBOOK_CLIENT_SECRET")
 
-  private def callbackUrl[T](implicit request: Request[T]) = {
+  private def callbackUrl[T](service: String)(implicit request: Request[T]) = {
     val protocol = if (request.secure) "https" else "http"
-    s"${protocol}://${request.host}/login/github/callback"
+    s"${protocol}://${request.host}/login/${service}/callback"
   }
 
   def github = Action { implicit request =>
-    val oauth = GitHubOAuthProvider(clientId, clientSecret, callbackUrl)
+    val oauth = GitHubOAuthProvider(githubClientId, githubClientSecret, callbackUrl("github"))
     Redirect(oauth.requestAccessUri("user", "repo"))
   }
   def githubCallback = Action.async { implicit request =>
-    val oauth = GitHubOAuthProvider(clientId, clientSecret, callbackUrl)
+    val oauth = GitHubOAuthProvider(githubClientId, githubClientSecret, callbackUrl("github"))
     val code: String = request.getQueryString("code").getOrElse("")
     oauth.requestToken(code).map(token =>
       Ok(views.html.index(token))
@@ -28,6 +30,14 @@ object Login extends Controller {
 
   def githubConfirm = Action {
     Ok(views.html.index("confirmed!!"))
+  }
+
+  def facebook = Action { implicit request =>
+    Redirect("http://url.to.redirect")
+  }
+
+  def facebookCallback = Action.async { implicit request =>
+    Ok(views.html.index("callback"))
   }
 
 }
